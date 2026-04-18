e3abdb2d79822413ac9480fda0d8e313bfa69b08
AbigailNazario <136490258+AbigailNazario@users.noreply.github.com>
2026-04-18 14:51:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  const API_URL = "http://localhost:3000";
+  const API_URL = "https://backend-filmeseseries.onrender.com";
 
   const lista = document.getElementById("lista");
   const form = document.getElementById("form");
