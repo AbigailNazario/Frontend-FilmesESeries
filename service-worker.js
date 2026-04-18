3b65f63b840c97ef1e263e22dd3eb58b6c36de10
AbigailNazario <136490258+AbigailNazario@users.noreply.github.com>
2026-04-18 15:38:57 -0300

    corrigindo erro do deploy

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
 const CACHE_NAME = "midias-cache-v1";
 
+const CACHE_NAME = "midias-cache-v2";
+
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -13,7 +15,16 @@ self.addEventListener("install", (e) => {
   );
 });
 
+// IGNORAR chamadas da API
 self.addEventListener("fetch", (e) => {
+  const url = e.request.url;
+
+  // se for API, NÃO intercepta
+  if (url.includes("/api/entries")) {
+    return;
+  }
+
+  // só cache para arquivos locais
   e.respondWith(
     caches.match(e.request).then((res) => {
       return res || fetch(e.request);
